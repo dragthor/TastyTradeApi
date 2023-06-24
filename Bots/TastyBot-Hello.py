49786baf1d34249e1188f0d1a354730e22eaa9da
Kristofer Krause <kris.krause@gmail.com>
2023-06-24 14:53:44 -0400

    hello bot

---
+++ Bots/TastyBot-Hello.py
@@ -0,0 +1,65 @@
+import sys
+sys.path.append('../')
+import TastyTradeApi
+
+email = "<YOUR_EMAIL>"
+password = "<YOUR_PASSWORD>"
+apiUrl = "https://api.tastyworks.com"
+authToken = ""
+userName = ""
+liveTradingEnabled = False
+
+authToken = TastyTradeApi.getSessionAuthorizationToken(apiUrl, email, password)
+
+if len(authToken) == 0:
+    print("Unable to get Tastytrade authorization token.")
+
+userName = TastyTradeApi.validateSession(apiUrl, authToken)
+
+if len(userName) == 0:
+    print("Unable to validate Tastytrade session.")
+
+accounts = TastyTradeApi.getCustomerAccounts(apiUrl, authToken)
+
+primaryAccount = accounts[0]["account"]["account-number"]
+
+balances = TastyTradeApi.getAccountBalances(apiUrl, authToken, primaryAccount)
+
+positions = TastyTradeApi.getAccountPositions(apiUrl, authToken, primaryAccount)
+
+print(
+    "----------------------------------------------------------------------------------------"
+)
+print(
+    "Account: "
+    + primaryAccount
+    + ", Cash: "
+    + balances["cash-balance"]
+    + ", Reg-T Margin: "
+    + balances["reg-t-margin-requirement"]
+    + ", Futures Margin: "
+    + balances["futures-margin-requirement"]
+)
+print(
+    "----------------------------------------------------------------------------------------"
+)
+
+positions.sort(key=lambda x: x["underlying-symbol"])
+
+for position in positions:
+    print(
+        position["underlying-symbol"]
+        + " "
+        + str(position["quantity"])
+        + " "
+        + position["symbol"]
+        + " "
+        + position["quantity-direction"]
+        + " "
+        + str(position["expires-at"])
+    )
+
+closeResult = TastyTradeApi.closeSession(apiUrl, authToken)
+
+if closeResult != 204:
+    print("Unable to close/kill Tastytrade session.")
---
+++ TastyTradeApi.py
@@ -0,0 +1,96 @@
+import requests
+import json
+
+
+def getSessionAuthorizationToken(apiUrl, email, password):
+    url = apiUrl + "/sessions"
+
+    payload = {"login": email, "password": password}
+    files = []
+    headers = {}
+
+    webResponse = requests.request(
+        "POST", url, headers=headers, data=payload, files=files
+    )
+
+    objJson = json.loads(webResponse.text)
+
+    return objJson["data"]["session-token"]
+
+
+def validateSession(apiUrl, token):
+    url = apiUrl + "/sessions/validate"
+
+    payload = {}
+    files = []
+    headers = {"Authorization": token}
+
+    webResponse = requests.request(
+        "POST", url, headers=headers, data=payload, files=files
+    )
+
+    objJson = json.loads(webResponse.text)
+
+    return objJson["data"]["username"]
+
+
+def closeSession(apiUrl, token):
+    url = apiUrl + "/sessions"
+
+    payload = {}
+    files = []
+    headers = {"Authorization": token}
+
+    webResponse = requests.request(
+        "DELETE", url, headers=headers, data=payload, files=files
+    )
+
+    return webResponse.status_code  # 204 status code is success
+
+
+def getCustomerAccounts(apiUrl, token):
+    url = apiUrl + "/customers/me/accounts"
+
+    payload = {}
+    files = []
+    headers = {"Authorization": token}
+
+    webResponse = requests.request(
+        "GET", url, headers=headers, data=payload, files=files
+    )
+
+    objJson = json.loads(webResponse.text)
+
+    return objJson["data"]["items"]
+
+
+def getAccountPositions(apiUrl, token, accountNumber):
+    url = apiUrl + "/accounts/" + accountNumber + "/positions"
+
+    payload = {}
+    files = []
+    headers = {"Authorization": token}
+
+    webResponse = requests.request(
+        "GET", url, headers=headers, data=payload, files=files
+    )
+
+    objJson = json.loads(webResponse.text)
+
+    return objJson["data"]["items"]
+
+
+def getAccountBalances(apiUrl, token, accountNumber):
+    url = apiUrl + "/accounts/" + accountNumber + "/balances"
+
+    payload = {}
+    files = []
+    headers = {"Authorization": token}
+
+    webResponse = requests.request(
+        "GET", url, headers=headers, data=payload, files=files
+    )
+
+    objJson = json.loads(webResponse.text)
+
+    return objJson["data"]
