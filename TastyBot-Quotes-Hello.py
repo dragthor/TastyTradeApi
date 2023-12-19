8354fd0d9494ba7c1f785b74cfd6fb4d67052881
Kristofer Krause <kris.krause@gmail.com>
2023-12-18 20:01:34 -0500

    greeks

--- TastyBot-Quotes-Hello.py
+++ TastyBot-Quotes-Hello.py
@@ -4,8 +4,12 @@ import time
 
 from DXFeed import *
 
+# https://symbol-lookup.dxfeed.com/
 
-tickers = [ "SPY" ]
+#tickers = [ ".SPY240119P420", ".SPY240216P420" ]
+
+tickers = [ ".SPX240119P4200", ".SPX240216P4200" ]
+
 
 liveTradingEnabled = False
 
@@ -18,11 +22,24 @@ async def main():
     userName = ""
 
     async def quote_callback(result):
-        ticker = result["data"][1][0] # Quote
-        bid = float(result["data"][1][6]) # bidPrice
-        ask = float(result["data"][1][10]) # askPrice
-        print(ticker + " - mid: " + str(midpoint(bid, ask)) + ", bid: " + str(bid) + ", ask: " + str(ask))
+        t = result["data"][0][0]
+        ticker = result["data"][1][0]
 
+        if t == "Quote":    
+            bid = float(result["data"][1][6]) # bidPrice
+            ask = float(result["data"][1][10]) # askPrice
+            print(ticker + " - mid: " + str(midpoint(bid, ask)) + ", bid: " + str(bid) + ", ask: " + str(ask))
+                
+        if t == "Greeks":
+            volatility = float(result["data"][1][7]) # volatility 
+            delta = float(result["data"][1][8]) # delta
+            theta = float(result["data"][1][10]) # theta
+            vega = float(result["data"][1][12]) # vega
+            gamma = float(result["data"][1][9]) # gamma
+            rho = float(result["data"][1][11]) # rho
+
+            print(ticker + " - d: " + str(delta) + " t: " + str(theta) + " v: " + str(vega) + " g: " + str(gamma) + " r: " + str(rho) + " iv: " + str(volatility))
+        
     def midpoint(bid, ask):
         return round((bid + (ask - bid) / 2), 2)
 
@@ -51,13 +68,14 @@ async def main():
 
     await tt_dxfeed.connect()
 
+
     # Utilize getFutureStreamerSymbols (or getEquityStreamerSymbol) for proper symbol.
     # items = TastyTradeApi.getFutureStreamerSymbols(apiUrl, authToken, tickers[0])
-    items = TastyTradeApi.getEquityStreamerSymbol(apiUrl, authToken, tickers[0])
+    # items = TastyTradeApi.getEquityStreamerSymbol(apiUrl, authToken, tickers[0])
 
     #print(items)
 
-    await tt_dxfeed.subscribe([DXEvent.QUOTE], tickers)
+    await tt_dxfeed.subscribe([DXEvent.QUOTE, DXEvent.GREEKS], tickers)
 
     seconds = 10
     t_end = time.time() + seconds
